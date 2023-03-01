f344f8191fa7d5342a9a59d6919dcda39478df47
Pablo Gillespie <pabloemanuelgille@gmail.com>
2023-03-01 15:18:30 -0300

    Deploy final

--- src/layouts/RutaProtegida.jsx
+++ src/layouts/RutaProtegida.jsx
@@ -3,28 +3,30 @@ import useAuth from "../hooks/useAuth";
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
 import Spinner from '../components/Spinner';
-
 const RutaProtegida = () => {
 
-    const { auth , cargando } = useAuth();
-
-    if(cargando) 'Cargando'
+    const { auth, cargando } = useAuth();
 
     return (
-        <>  
-            {auth?._id ? (
-                <div className="bg-gray-100">
-                    <Header />
+        <>
+            {cargando ? <Spinner /> : (
+                <>
+                    {auth?._id ? (
+                        <div className="bg-gray-100">
+                            <Header />
 
-                    <div className="md:flex md:min-h-screen">
-                        <Sidebar />
+                            <div className="md:flex md:min-h-screen">
+                                <Sidebar />
 
-                        <main className="flex-1 p-10">
-                            <Outlet />
-                        </main>
-                    </div>
-                </div>
-            ) : <Navigate to={'/'}/>}
+                                <main className="flex-1 p-10">
+                                    <Outlet />
+                                </main>
+                            </div>
+                        </div>
+                    ) : <Navigate to={'/'}/>}
+                </>
+            )}
+            
         </>
     )
 }
